6142f4e795909e3e6a0a0e4188be745fc314cfbf
Nova <novachow@vip.qq.com>
2018-04-18 10:21:08 +0800

    xxx

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 周新星 on 2018/4/18.
+//  Copyright © 2018年 周新星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 周新星 on 2018/4/18.
+//  Copyright © 2018年 周新星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

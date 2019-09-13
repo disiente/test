9e29d84942e30d133b7abb2076df855915e88975
Joe <iosqxx@163.com>
2019-09-13 21:46:45 +0800

    test

--- test/test/AppDelegate.h
+++ test/test/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  test
 //
-//  Created by 周新星 on 2018/4/18.
-//  Copyright © 2018年 周新星. All rights reserved.
+//  Created by nova on 2018/4/18.
+//  Copyright © 2018年 nova. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -2,8 +2,8 @@
 //  ViewController.m
 //  test
 //
-//  Created by 周新星 on 2018/4/18.
-//  Copyright © 2018年 周新星. All rights reserved.
+//  Created by nova on 2018/4/18.
+//  Copyright © 2018年 nova. All rights reserved.
 //
 
 #import "ViewController.h"
